5ae51cc285a81cf4704169a431d02e30d4acbd61
Natalia Kazakova <natalia.kazakova@devexpress.com>
2022-09-29 12:18:54 +0400

    update script code

--- CS/WebDashboard_SetMasterFilter/Default.aspx
+++ CS/WebDashboard_SetMasterFilter/Default.aspx
@@ -9,9 +9,8 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div id="setMasterFilterButton" style="float: left; margin-left: 150px;"></div>
-        <div id="clearMasterFilterButton" style="float: left; margin-left: 150px;"></div>
-
+        <div id="setMasterFilterButton" style="float: left; margin-left: 10px;"></div>
+        <div id="clearMasterFilterButton" style="float: left; margin-left: 20px;"></div>
         <div style="position: absolute; left: 0; right: 0; top:50px; bottom:0;">
             <dx:ASPxDashboard ID="ASPxDashboard1" runat="server" WorkingMode="Viewer" ClientInstanceName="webDashboard"
                 Width="100%" Height="100%" AccessKey="q">
--- VB/WebDashboard_SetMasterFilter/Scripts/MasterFiltering.js
+++ VB/WebDashboard_SetMasterFilter/Scripts/MasterFiltering.js
@@ -1,42 +1,14 @@
-function onBeforeRender(s) {
-    let dashboardControl = s.GetDashboardControl();
-    let viewerApiExtension = dashboardControl.findExtension('viewerApi');
+var dashboardControl;
+var viewerApiExtension;
 
-    initializeFilters(viewerApiExtension);
+function onBeforeRender(s) {
+    dashboardControl = s.GetDashboardControl();
+    viewerApiExtension = dashboardControl.findExtension('viewerApi');
+    if (dashboardControl)
+        dashboardControl.on('dashboardEndUpdate', initializeControls);
 }
 
-function initializeFilters(viewerApiExtension) {
-    function applyFilters() {
-        var selectedValues = [['UK', 'Anne Dodsworth'], ['USA', 'Andrew Fuller']];
-        var selectedRange = { minimum: new Date(2015, 1, 1), maximum: new Date(2015, 12, 31) };
-
-        setMasterFilter('gridDashboardItem1', selectedValues);
-        setRange('rangeFilterDashboardItem1', selectedRange);
-    }
-
-    function setMasterFilter(itemName, selectedValues) {
-        if (viewerApiExtension.canSetMasterFilter(itemName)) {
-            viewerApiExtension.setMasterFilter(itemName, selectedValues);
-        }
-    }
-
-    function setRange(itemName, selectedRange) {
-        if (viewerApiExtension.canSetMasterFilter(itemName)) {
-            viewerApiExtension.setRange(itemName, selectedRange);
-        }
-    }
-
-    function clearFilters() {
-        clearMasterFilter('gridDashboardItem1');
-        clearMasterFilter('rangeFilterDashboardItem1');
-    }
-
-    function clearMasterFilter(itemName) {
-        if (viewerApiExtension.canClearMasterFilter(itemName)) {
-            viewerApiExtension.clearMasterFilter(itemName);
-        }
-    }
-
+function initializeControls() {
     $("#setMasterFilterButton").dxButton({
         text: "Apply Filters",
         onClick: applyFilters
@@ -47,3 +19,24 @@ function initializeFilters(viewerApiExtension) {
         onClick: clearFilters
     });
 };
+
+function applyFilters() {
+    var selectedValues = [['UK', 'Anne Dodsworth'], ['USA', 'Andrew Fuller']];
+    var selectedRange = { minimum: new Date(2015, 1, 1), maximum: new Date(2015, 12, 31) };
+
+    if (viewerApiExtension.canSetMasterFilter('gridDashboardItem1')) {
+        viewerApiExtension.setMasterFilter('gridDashboardItem1', selectedValues);
+    }
+    if (viewerApiExtension.canSetMasterFilter('rangeFilterDashboardItem1')) {
+        viewerApiExtension.setRange('rangeFilterDashboardItem1', selectedRange);
+    }
+}
+
+function clearFilters() {
+    if (viewerApiExtension.canClearMasterFilter('gridDashboardItem1')) {
+        viewerApiExtension.clearMasterFilter('gridDashboardItem1');
+    }
+    if (viewerApiExtension.canClearMasterFilter('rangeFilterDashboardItem1')) {
+        viewerApiExtension.clearMasterFilter('rangeFilterDashboardItem1');
+    }
+}
